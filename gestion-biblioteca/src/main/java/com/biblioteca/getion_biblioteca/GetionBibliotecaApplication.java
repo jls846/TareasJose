1247190bdca153afe94ebbc7bff2a4dc4f85b9bb
jls846 <jlussanchez123@gmail.com>
2026-02-27 22:12:40 -0600

    Configuracion de .properties

---
+++ gestion-biblioteca/src/main/java/com/biblioteca/getion_biblioteca/GetionBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.getion_biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetionBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetionBibliotecaApplication.class, args);
+	}
+
+}
